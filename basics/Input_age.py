c13db608ce30880f4ed8aa68c0ecc3da6a0444e8
SAHITH-KAJA-KHAN <sahithm88@gmail.com>
2025-01-06 11:36:25 +0530

    TellAge

---
+++ basics/Input_age.py
@@ -0,0 +1,8 @@
+# Create a program that asks the user to enter their name and their age.
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+
+name = input("Enter your name")
+age = int(input("Enter your age"))
+
+print("you will be 100 in",2025 + (100- age))
