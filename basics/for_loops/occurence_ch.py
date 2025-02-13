13e7a90b6858086590ac4fc36fc66f2cdb3633b6
SAHITH-KAJA-KHAN <sahithm88@gmail.com>
2025-02-13 10:01:24 +0530

    occurenceOfcharacters

---
+++ basics/for_loops/occurence_ch.py
@@ -0,0 +1,15 @@
+# x = input("enter")
+# y = input("enter target character")
+# print(x)
+# x = x.lower()
+# c=0
+# for i in range(len(x)):
+#   if x[i] == y:
+#     c+=1
+    
+# print(c)
+
+x= input("enter target word ")
+y= input("enter target character ")
+res = x.count(y)
+print(res)
