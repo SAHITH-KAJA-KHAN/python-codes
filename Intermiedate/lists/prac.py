8ae65a17c6342fa54bcd3901114c03686d104e4f
SAHITH-KAJA-KHAN <sahithm88@gmail.com>
2025-02-13 06:52:35 +0530

    for prac

---
+++ Intermiedate/lists/prac.py
@@ -0,0 +1,40 @@
+# list = [1,2,3,4,5]
+# lst= [5,6,7,8,9,10,11,12,13]
+# list.append(1)
+# print (list)
+# list.sort()
+# print (list)
+# # list.reverse()
+# # print (list)
+# list.remove(1)
+# print (list)
+# list.remove(1)
+# print (list)
+# print(max(list))
+# list.insert(1,7) #index , what to print
+# print (list)
+# list.extend([11,22,33])
+# print (list)
+# list.extend([11,22,33])
+# print (list)
+# list.pop() #default aa last element, if you want to pop somethoing add thats index
+# list.extend(lst)
+# print (list)
+
+
+
+
+
+
+
+s = ['1', '2', '3', '4']
+print(type(s))
+res = map(int, s)
+print(list(res))
+
+
+
+
+
+
+
