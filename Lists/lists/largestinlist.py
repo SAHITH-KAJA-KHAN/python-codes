2961bb3532be7c60643c2d5cb0a3738af318ad05
SAHITH-KAJA-KHAN <sahithm88@gmail.com>
2025-02-20 07:19:02 +0530

    *****     *****     *****     *****     *****

--- Lists/lists/largestinlist.py
+++
@@ -1,6 +0,0 @@
-x=input().split()
-for i in range(len(x)):  # Step 2: Loop through the list using index
-    x[i] = int(x[i])  # Convert each element to an integer and store it back
-print(x)
-print(max(x))  # Step 3: Print the list of integers
-
---
+++ Patterns/p-A.py
@@ -0,0 +1,16 @@
+'''         where n =5 
+    *****
+    *****
+    *****
+    *****
+    *****     '''
+n = int(input())
+for i in range(n):
+  print("*" *n)
+print()
+for i in range(n):
+  for j in range(n):
+    print("*" ,end="")
+    
+  print()
+  
