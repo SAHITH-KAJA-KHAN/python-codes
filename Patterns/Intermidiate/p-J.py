86c1f5461da2cde5a6732dffe65c4324adcce0f4
SAHITH-KAJA-KHAN <sahithm88@gmail.com>
2025-02-20 15:22:41 +0530

    *     **     ***     ****     *****     ****     ***     **     *

--- Patterns/Intermidiate/p-J.py
+++ Patterns/Intermidiate/p-J.py
@@ -1,3 +1,5 @@
+
+
 ''' *
     **
     ***
@@ -8,3 +10,11 @@
     **
     *
 '''
+
+n = int(input())  # Taking user input
+
+for row in range(1, (2 * n)):  
+    colTimes = row if row <= n else (2 * n) - row  
+    for col in range(colTimes):  
+        print("*", end="")  
+    print() 
