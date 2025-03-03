eefe86fad4f46014b9968051081d40ea66918b9a
SAHITH-KAJA-KHAN <sahithm88@gmail.com>
2025-03-03 17:45:12 +0530

    changed

--- new_Learnings/string_vowel.py
+++ new_Learnings/string_vowel.py
@@ -1,8 +1,8 @@
 st = input()
 for i in st:
-    if i in "aeiou AEIOU":
-        print(i, end = ' ')
-        print()
-    elif i not in "aeiou AEIOU":
-        print(i, end = ' ')
+    if i not in "aeiou AEIOU":
+        print(i,end="")
+        # print()
+    # elif i not in "aeiou AEIOU":
+    #     print(i, end = ' ')
       
