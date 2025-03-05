a02961e931021caf6baad5631fce68fe642c4bd3
SAHITH-KAJA-KHAN <sahithm88@gmail.com>
2025-03-05 15:03:00 +0530

    no+str

--- numerical_probs/no+str.py
+++ numerical_probs/no+str.py
@@ -1,9 +1,23 @@
+# import re
 
-#input : 1abc23
-#output : 24
+# s = input()
+# numbers = re.findall(r'\d+', s)  # Find all numbers in the string
+# total_sum = sum(map(int, numbers))  # Convert to int and sum them
+# print(total_sum)
+
 s = input()
-sum = 0
-for i in s:
-    if i.isdigit():
-        sum += int(i)
-print(sum)
+num = ""
+total_sum = 0
+
+for char in s:
+    if char.isdigit():
+        num += char  # Build the number
+    else:
+        if num:
+            total_sum += int(num)  # Add the complete number
+            num = ""  # Reset for the next number
+
+if num:
+    total_sum += int(num)  # Add the last number if any
+
+print(total_sum)
