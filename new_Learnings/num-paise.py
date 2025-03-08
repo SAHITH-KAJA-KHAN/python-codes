540fd67506bdb6ba217a1613357b0ccb2db9a391
SAHITH-KAJA-KHAN <sahithm88@gmail.com>
2025-03-08 11:14:36 +0530

    num-paise.

---
+++ new_Learnings/num-paise.py
@@ -0,0 +1,8 @@
+amount = int(input())
+denominations = [500, 100, 50, 20, 10, 5, 2]
+
+for denom in denominations:
+    count = amount // denom  
+    print(denom, "-", count)
+    amount %= denom
+    
