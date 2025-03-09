2866cd76995de4946cce6b911c164482b6528651
SAHITH-KAJA-KHAN <sahithm88@gmail.com>
2025-03-09 18:38:42 +0530

    numerical_probs/oct2bi.py

---
+++ numerical_probs/oct2bi.py
@@ -0,0 +1,76 @@
+# octal = input("Enter an octal number: ")
+# decimal = int(octal, 8)
+# binary = bin(decimal)[2:]
+# print("Binary:", binary)
+
+from num2words import num2words
+
+# Most common usage.
+print(num2words(11098))
+
+# can also specify this for -th format
+print(num2words(11098, to='ordinal'))
+
+
+# Python program to convert number into words by breaking 
+# it into groups of three
+
+def convertToWords(n):
+    if n == 0:
+        return "Zero"
+    
+    # Words for numbers 0 to 19
+    units = [
+        "",        "One",       "Two",      "Three",
+        "Four",    "Five",      "Six",      "Seven",
+        "Eight",   "Nine",      "Ten",      "Eleven",
+        "Twelve",  "Thirteen",  "Fourteen", "Fifteen",
+        "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+    ]
+    
+    # Words for numbers multiple of 10        
+    tens = [
+        "",     "",     "Twenty",  "Thirty", "Forty",
+        "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+    ]
+    
+    multiplier = ["", "Thousand", "Million", "Billion"]
+    
+    res = ""
+    group = 0
+    
+    # Process number in group of 1000s
+    while n > 0:
+        if n % 1000 != 0:
+            
+            value = n % 1000
+            temp = ""
+            
+            # Handle 3 digit number
+            if value >= 100:
+                temp = units[value // 100] + " Hundred "
+                value %= 100
+
+            # Handle 2 digit number
+            if value >= 20:
+                temp += tens[value // 10] + " "
+                value %= 10
+
+            # Handle unit number
+            if value > 0:
+                temp += units[value] + " "
+
+            # Add the multiplier according to the group
+            temp += multiplier[group] + " "
+            
+            # Add the result of this group to overall result
+            res = temp + res
+        n //= 1000
+        group += 1
+    
+    # Remove trailing space
+    return res.strip()
+
+if __name__ == "__main__":
+    n = 2147483647
+    print(convertToWords(n))
